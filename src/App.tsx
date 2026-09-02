ff04b1faa87f78bc99ebc24d7df0fe9f1cb11c60
Sujan299 <mrsujan321@gmail.com>
2026-09-02 08:15:38 +0545

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
-export default function App(){
-  return <div className='text-red-500'>Hello app</div>
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Navbar } from './components/Navbar'
+
+function App() {
+  return (
+        <BrowserRouter>
+          <div className="min-h-screen bg-gray-50">
+            <Navbar />
+            
+          </div>
+        </BrowserRouter>
+  )
 }
+
+export default App
